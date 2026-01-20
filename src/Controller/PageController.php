19758ce5de6eab05be2a1a295f727ad89e79aec1
julio <julpermaz@alu.edu.gva.es>
2026-01-20 07:11:49 +0000

    posts

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
-use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Post;
 use App\Entity\Contact;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Attribute\Route;
+use App\Form\PostFormType;
 use App\Form\ContactFormType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class PageController extends AbstractController
 {
@@ -31,10 +35,17 @@ final class PageController extends AbstractController
     }
 
     #[Route('/blog', name: 'blog')]
-    public function blog(): Response
+    public function blog(ManagerRegistry $doctrine): Response
     {
-        return $this->render('page/blog.html.twig', []);
+        $repository = $doctrine->getRepository(Post::class);
+        $posts = $repository->findAll();
+
+        
+        return $this->render('page/blog.html.twig', [
+            'posts' => $posts,
+        ]);
     }
+
 
     #[Route('/contact', name: 'contact')]
     public function contact(ManagerRegistry $doctrine, Request $request): Response
@@ -68,4 +79,71 @@ final class PageController extends AbstractController
     {
         return $this->render('page/components.html.twig', []);
     }
+
+#[Route('/blog/new', name: 'new_post')]
+    public function newPost(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostFormType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // 1. Recogemos los datos básicos (Título, Contenido...)
+            $post = $form->getData();   
+            
+            /** @var UploadedFile $imageFile */
+            $imageFile = $form->get('image')->getData();
+
+            // Si el usuario ha subido una imagen, la procesamos
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // Usamos el slugger para limpiar el nombre del archivo (quita espacios, acentos...)
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                // Movemos el archivo a la carpeta public/images/blog
+                try {
+                    $imageFile->move(
+                        $this->getParameter('kernel.project_dir').'/public/images/blog',
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // Aquí podrías añadir un mensaje de error si falla la subida
+                }
+
+                // Guardamos SOLO el nombre del archivo en la base de datos
+                $post->setImage($newFilename);
+            }
+            // -----------------------------------
+
+            $post->setSlug($slugger->slug($post->getTitle()));
+            $post->setPostUser($this->getUser());
+            $post->setNumLikes(0);
+            $post->setNumComments(0);
+
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('single_post', ["slug" => $post->getSlug()]);
+
+        }
+
+        return $this->render('blog/new_post.html.twig', array(
+            'form' => $form->createView()    
+        ));
+    }
+
+    #[Route('/single_post/{id}', name: 'single_post')]
+    public function post(ManagerRegistry $doctrine, $id): Response
+    {
+        $repositorio = $doctrine->getRepository(Post::class);
+        
+        $post = $repositorio->find($id);
+        
+        return $this->render('blog/single_post.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
 }
---
+++ src/Form/PostFormType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+
+
+class PostFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('image', FileType::class,['attr' => ['class'=>'form-control']],[
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image file',
+                    ])
+                ],
+            ])
+            ->add('Send', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
